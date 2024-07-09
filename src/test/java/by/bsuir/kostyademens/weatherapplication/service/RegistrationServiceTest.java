9dde7de287f924248fb251d6741afe7296b19614
KostyaDemens <kostyademens@gmail.com>
2024-07-09 22:19:14 +0300

    Deleted all tests

--- src/test/java/by/bsuir/kostyademens/weatherapplication/service/RegistrationServiceTest.java
+++ src/test/java/by/bsuir/kostyademens/weatherapplication/service/RegistrationServiceTest.java
@@ -1,51 +1,17 @@
 package by.bsuir.kostyademens.weatherapplication.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
-import by.bsuir.kostyademens.weatherapplication.dao.UserDao;
-import by.bsuir.kostyademens.weatherapplication.exception.UserAlreadyExistsException;
-import by.bsuir.kostyademens.weatherapplication.model.User;
-import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
-class RegistrationServiceTest {
+public class RegistrationServiceTest {
 
-    @InjectMocks
     private RegistrationService registrationService;
-
-    @Mock
-    private UserDao userDao;
-
-
-    private User user;
 
     @BeforeEach
     void prepare() {
-        user = new User("Vova", "123");
+        registrationService = new RegistrationService();
     }
 
-    @Test
-    void registerUser_ShouldThrowUserAlreadyExistsException() {
-        when(userDao.findByLogin(user.getEmail())).thenReturn(Optional.of(user));
-
-        assertThrows(UserAlreadyExistsException.class, () -> registrationService.register(user));
-    }
-
-    @Test
-    void registerUser_ShouldCreateNewUserSuccessfully() {
-        when(userDao.findByLogin(user.getEmail())).thenReturn(Optional.empty());
-
-        registrationService.register(user);
-
-        verify(userDao, times(1)).save(user);
-    }
-
-
+//    @Test
+//    void throwExcep
 }
